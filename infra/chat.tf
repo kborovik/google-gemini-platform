8ee58559524ad1377c0c2a886eabb176131f32a3
Konstantin Borovik <github@lab5.ca>
2026-09-24 10:36:33 -0400

    chore(infra): disable cloud run domain mapping for chat service

--- infra/chat.tf
+++ infra/chat.tf
@@ -156,18 +156,18 @@ resource "google_cloud_run_v2_service" "chat" {
   ]
 }
 
-resource "google_cloud_run_domain_mapping" "chat" {
-  location = var.region
-  name     = "credit-policy.ai.lab5.ca"
-  project  = var.project
-
-  metadata {
-    namespace = var.project
-  }
-
-  spec {
-    route_name = google_cloud_run_v2_service.chat.name
-  }
-
-  depends_on = [google_project_service.apis]
-}
+# resource "google_cloud_run_domain_mapping" "chat" {
+#   location = var.region
+#   name     = "credit-policy.ai.lab5.ca"
+#   project  = var.project
+#
+#   metadata {
+#     namespace = var.project
+#   }
+#
+#   spec {
+#     route_name = google_cloud_run_v2_service.chat.name
+#   }
+#
+#   depends_on = [google_project_service.apis]
+# }
